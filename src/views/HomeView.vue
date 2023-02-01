e0585c14d51c246c946ef1cb597908e7ea84a600
juliatarasow <juliatarasow@web.de>
2023-02-01 16:58:48 +0100

    component example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,19 @@
     <h1>
       Frontends we like!
     </h1>
+
+  <HelloWorld/>
   </main>
 </template>
 
 
 <script>
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default {
+    components: {
+    HelloWorld,
+  },
+}
 
 </script>
