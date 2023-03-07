b64d0c6359ccebd3cc6bec109399b3fe5fbed597
judithcrasser <judith.crasser@web.de>
2023-03-07 14:11:13 +0100

    expanded db categories

--- src/components/Accordion/Accordion.vue
+++ src/components/Accordion/Accordion.vue
@@ -55,7 +55,7 @@
                             <p class="text">
                                 {{ musician.description }}
                             </p>
-                            <p class="text">
+                            <!-- <p class="text">
                                 Some of my favourite pieces are:
                             </p>
                             <ul class="list">
@@ -68,7 +68,7 @@
                                 <li>
                                     Symphony No. 7
                                 </li>
-                            </ul>
+                            </ul> -->
                         </div>
 
                     </div>
