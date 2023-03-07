5ee2cbb837a7f212ce0e7736f133ee7d0d8a04a5
judithcrasser <judith.crasser@web.de>
2023-03-07 15:55:25 +0100

    gallery

---
+++ src/components/Gallery/Gallery.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <h1>Gallery</h1>
+        <!-- <div v-for="tag in tags" :key="tag">
+            <router-link :to="{ name: 'Tag', params: { tag }}">
+                {{ tag }}
+            </router-link>
+        </div> -->
+        <div v-if="error">
+            {{ error }}
+        </div>
+        <div class="img-container">
+            <div 
+                v-for="musician in musicians"
+                :key="musician.id"
+                class="img-box">
+                <img 
+                :src="musician.image"
+                :alt="musician.name"
+                >
+            </div>
+            
+        </div>
+        <!-- <div v-else>
+            Loading...
+        </div> -->
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            musicians: [],
+        };
+    },
+    mounted() {
+        fetch('http://localhost:3000/musicians')
+            .then(res => res.json())
+            .then(data => this.musicians = data)
+            .catch(err => console.log(err.message))
+    }
+}
+</script>
+
+<style lang="scss">
+
+h1 {
+    padding: 2.5rem 0 0 2.5rem;
+}
+
+.img-container {
+    padding: 2rem;
+    display: flex;
+    flex-wrap: wrap;
+
+    .img-box {
+    padding: 0.5rem;
+    display: flex;
+
+    img {
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+    filter: grayscale(100%);
+
+    &:hover {
+        cursor: pointer;
+    }
+   }
+  }
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <HeroSection />
   <Accordion />
   <AppTab />
+  <Gallery />
 </template>
 
 
@@ -9,12 +10,15 @@
 import Accordion from '../components/Accordion/Accordion.vue'
 import AppTab from '../components/AppTab.vue'
 import HeroSection from '../components/HeroSection.vue';
+import Gallery from '../components/Gallery/Gallery.vue'
+
 
 export default {
   components: {
     Accordion,
     AppTab,
-    HeroSection
+    HeroSection,
+    Gallery
   },
 }
 
